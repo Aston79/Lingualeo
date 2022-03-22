fbeb763e7c15804bb5155d1ec962a396cd3940eb
Viktor <Aston79.vt@gmail.com>
2022-03-22 22:21:00 +0300

    add Test check that music link exist

--- src/test/java/pages/JunglePage.java
+++ src/test/java/pages/JunglePage.java
@@ -15,9 +15,9 @@ public class JunglePage extends BasePage {
 
     public static final SelenideElement JUNGLE = $(By.xpath("//*[@id=\"topitem-jungle\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_MUSIC = $(By.xpath("//*[@id=\"topitem-jungle-music\"]/a/span/span[2]"));
-    public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC = $(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div[1]/div/div/div/ul/li[3]/div/a/div/div[2]"));
-    public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC_MADONNA = $(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[3]/div[1]/div/div/li[1]/div/a/div/div[2]"));
-    public static final SelenideElement MADONNA_YOUTUBE_LINK = $(By.xpath("//*[@id=\"movie_player\"]/a/div/div[1]"));
+    public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC = $(By.xpath("//*[text()=\"Electronic\"]"));
+    public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC_MADONNA = $(By.xpath("//h2[contains(.,'Madonna - Frozen')]"));
+    public static final SelenideElement MADONNA_VIDEO_TITLE = $(By.xpath("//span[contains(text(),'Madonna')]"));
     public static final SelenideElement JUNGLE_MOVIES_AND_VIDEOS = $(By.xpath("//*[@id=\"topitem-jungle-video\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_BOOKS_AND_ARTICLES = $(By.xpath("//*[@id=\"topitem-jungle-books\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_COLLECTIONS = $(By.xpath("//*[@id=\"topitem-jungle-collections\"]/a/span/span[2]"));
@@ -26,7 +26,7 @@ public class JunglePage extends BasePage {
 
     public void checkThatMusicLinkExist(){
         $(JUNGLE).click();
-        $(JUNGLE_MUSIC).click();
+//        $(JUNGLE_MUSIC).click();
         $(JUNGLE_MUSIC_ELECTRONIC).click();
         $(JUNGLE_MUSIC_ELECTRONIC_MADONNA).click();
     }
--- src/test/java/tests/JungleTest.java
+++ src/test/java/tests/JungleTest.java
@@ -1,19 +1,18 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static pages.JunglePage.MADONNA_YOUTUBE_LINK;
+import static pages.JunglePage.MADONNA_VIDEO_TITLE;
 
-public class JungleTest extends BaseTest{
+public class JungleTest extends BaseTest {
 
     @Test
-    public void checkThatMusicLinkExistTest(){
+    public void checkThatMusicLinkExistTest() {
         loginPage.openPage();
         loginPage.login(username, password);
         junglePage.checkThatMusicLinkExist();
-        $(MADONNA_YOUTUBE_LINK).shouldBe(visible);
+        $(MADONNA_VIDEO_TITLE).shouldBe(visible);
     }
 }
