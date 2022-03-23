1fee0186bbf7b75a5ee363655c01b35402915aad
Viktor <Aston79.vt@gmail.com>
2022-03-23 09:16:15 +0300

    add Test check that movie and video link exist

--- src/test/java/pages/JunglePage.java
+++ src/test/java/pages/JunglePage.java
@@ -14,11 +14,13 @@ public class JunglePage extends BasePage {
     }
 
     public static final SelenideElement JUNGLE = $(By.xpath("//*[@id=\"topitem-jungle\"]/a/span/span[2]"));
-    public static final SelenideElement JUNGLE_MUSIC = $(By.xpath("//*[@id=\"topitem-jungle-music\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC = $(By.xpath("//*[text()=\"Electronic\"]"));
     public static final SelenideElement JUNGLE_MUSIC_ELECTRONIC_MADONNA = $(By.xpath("//h2[contains(.,'Madonna - Frozen')]"));
     public static final SelenideElement MADONNA_VIDEO_TITLE = $(By.xpath("//span[contains(text(),'Madonna')]"));
-    public static final SelenideElement JUNGLE_MOVIES_AND_VIDEOS = $(By.xpath("//*[@id=\"topitem-jungle-video\"]/a/span/span[2]"));
+//    public static final SelenideElement JUNGLE_MOVIES_AND_VIDEOS = $(By.xpath("//span[contains(.,'Movies and videos')]"));
+    public static final SelenideElement JUNGLE_MOVIES_AND_VIDEOS_CARTOONS = $(By.xpath("//h2[contains(.,'Cartoons')]"));
+    public static final SelenideElement JUNGLE_CARTOONS_DARK_KNIGHT = $(By.xpath("//h2[contains(.,'How The Dark Knight Should Have Ended')]"));
+    public static final SelenideElement TITLE_OF_DARK_KNIGHT_CARTOON = $(By.xpath("//span[contains(.,'How The Dark Knight Should Have Ended')]"));
     public static final SelenideElement JUNGLE_BOOKS_AND_ARTICLES = $(By.xpath("//*[@id=\"topitem-jungle-books\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_COLLECTIONS = $(By.xpath("//*[@id=\"topitem-jungle-collections\"]/a/span/span[2]"));
     public static final SelenideElement JUNGLE_MY_CONTENT = $(By.xpath("//*[@id=\"topitem-jungle-my\"]/a/span/span[2]"));
@@ -26,8 +28,14 @@ public class JunglePage extends BasePage {
 
     public void checkThatMusicLinkExist(){
         $(JUNGLE).click();
-//        $(JUNGLE_MUSIC).click();
         $(JUNGLE_MUSIC_ELECTRONIC).click();
         $(JUNGLE_MUSIC_ELECTRONIC_MADONNA).click();
+    }
+
+    public void checkThatMoviesAndVideoLinkExistTest(){
+        $(JUNGLE).click();
+//        $(JUNGLE_MOVIES_AND_VIDEOS).click();
+        $(JUNGLE_MOVIES_AND_VIDEOS_CARTOONS).click();
+        $(JUNGLE_CARTOONS_DARK_KNIGHT).click();
     }
 }
--- src/test/java/tests/JungleTest.java
+++ src/test/java/tests/JungleTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static pages.JunglePage.MADONNA_VIDEO_TITLE;
+import static pages.JunglePage.TITLE_OF_DARK_KNIGHT_CARTOON;
 
 public class JungleTest extends BaseTest {
 
@@ -14,5 +15,13 @@ public class JungleTest extends BaseTest {
         loginPage.login(username, password);
         junglePage.checkThatMusicLinkExist();
         $(MADONNA_VIDEO_TITLE).shouldBe(visible);
+    }
+
+    @Test
+    public void checkThatMoviesAndVideoLinkExistTest() {
+        loginPage.openPage();
+        loginPage.login(username, password);
+        junglePage.checkThatMoviesAndVideoLinkExistTest();
+        $(TITLE_OF_DARK_KNIGHT_CARTOON).shouldBe(visible);
     }
 }
